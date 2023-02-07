b4b16132468c97f15d18bee059bf6e4610545223
felicia <feli5659@stud.kea.dk>
2023-02-07 20:42:28 +0100

    typewriter add

---
+++ typewriter/typewriter.js
@@ -0,0 +1,48 @@
+"use strict";
+
+let iterator = 0;
+
+let letter;
+const typewriter = document.querySelector("#typewriter");
+let typewriterText = typewriter.textContent;
+const maxNumberOfItems = typewriterText.length;
+// empties #typewriter content before the loop
+typewriter.textContent = "";
+
+
+initLoop();
+
+function initLoop() {
+  loop();
+}
+
+function loop() {
+  iterator++;
+  letter = "";
+
+  // this loops through each letter with delay of 200 and
+  if (iterator <= maxNumberOfItems) {
+    setTimeout(loop, 200);
+    letter = typewriterText[iterator - 1];
+    typewriter.textContent += letter;
+    if (letter == " ") {
+      // Audio #typespace
+      document.querySelector("#typespace").volume = 1;
+      document.querySelector("#typespace").currentTime = 0;
+      document.querySelector("#typespace").play();
+    } else {
+      //   Audio #typekey1
+      document.querySelector("#typekey1").volume = 1;
+      document.querySelector("#typekey1").currentTime = 0;
+      document.querySelector("#typekey1").play();
+      // document.querySelector("#typekey2").volume = 1;
+      // document.querySelector("#typekey2").currentTime = 0;
+      // document.querySelector("#typekey2").play();
+    }
+    // audio return at end
+    document.querySelector("#typereturn").volume = 1;
+    document.querySelector("#typereturn").currentTime = 0;
+    document.querySelector("#typereturn").play();
+    console.log(letter);
+  }
+}
