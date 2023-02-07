67c8e34344e8eebe68c3f90605b368da965d5c76
felicia <feli5659@stud.kea.dk>
2023-02-07 12:12:39 +0100

    prints to output

--- bigfatstring/string.js
+++ bigfatstring/string.js
@@ -11,6 +11,7 @@ let result;
 let input;
 let option;
 let output;
+let passWord = "*";
 
 // loaded function
 window.addEventListener("loaded", start());
@@ -23,50 +24,59 @@ function start() {
 function generateOutput() {
   //   console.log("hej");
   input = inputField.value;
-  output = outputField.value;
 
   //   Number is added to this as we want it to read the field as a number NOT a string
   option = Number(choice.value);
-  if (option === 0) {
+  if (option === 1) {
     // If input is a first name: Make the first character in a name uppercase, and the rest lowercase
 
-    result = input.charAt(0).toUpperCase() + input.substring(1).toLowerCase();
-
-    console.log(result);
-  } else if (option === 1) {
+    outputField.value = input.charAt(0).toUpperCase() + input.substring(1).toLowerCase();
+    console.log("option 1");
+  } else if (option === 2) {
     // If input is a full name: Find the first name
 
-    result = input.substring(0, input.indexOf(" "));
-    console.log(result);
-  } else if (option === 2) {
+    outputField.value = input.substring(0, input.indexOf(" "));
+    console.log("option 2");
+  } else if (option === 3) {
     // If input is a full name: Find the length of the first name
 
-    result = input.substring(0, input.indexOf(" ")).length;
-
-    console.log(result);
-  } else if (option === 3) {
+    outputField.value = input.substring(0, input.indexOf(" ")).length;
+    console.log("option 3");
+  } else if (option === 4) {
     // If input is a full name: Find the middle name start and end position, and the middle name itself in a full name string
 
-    result = input.substring(input.indexOf(" ") + 1, input.lastIndexOf(" "));
-    console.log(result);
-  } else if (option === 4) {
-    // If input is a filename: Check if a filename is .png or .jpg
-
-    result = input.endsWith(".jpg");
-    //  result = input.endsWith(".png");
-    console.log(result);
+    outputField.value = input.substring(input.indexOf(" ") + 1, input.lastIndexOf(" "));
+    console.log("option 4");
   } else if (option === 5) {
-    // If input is a password: Hide a password with the correct number of *s
-    result = input.replace(input.substring(0), "*");
-    console.log(result);
+    // If input is a filename: Check if a filename is .png or .jpg
+    if ((result = input.endsWith(".jpg"))) {
+      outputField.value = result = input.endsWith(".jpg");
+    } else if ((result = input.endsWith(".png"))) {
+      outputField.value = result = input.endsWith(".png");
+    } else {
+      outputField.value = "There is no such file";
+    }
+    console.log("option 5");
   } else if (option === 6) {
+    // If input is a password: Hide a password with the correct number of *s
+    // outputField.value = input.replace(input.substring(0), "*");
+    outputField.value = `${passWord.repeat(input.length)} (This passwords length is ${input.length} stars :-))`;
+    console.log("option 6");
+  } else if (option === 7) {
     // With any input: Make the third character uppercase
 
-    result = input.substring(0, 2) + input.charAt(2).toUpperCase() + input.substring(3);
-    console.log(result);
-  } else if (option === 7) {
+    outputField.value = input.substring(0, 2) + input.charAt(2).toUpperCase() + input.substring(3);
+    console.log("option 7");
+  } else if (option === 8) {
     // With any input: Make a character uppercase, if it follows a space or a hyphen
-
+    result = "";
+    Array.from(input).forEach((letter, i) => {
+      if (input[1 - 1] == " " || input[i - 1] === "-") {
+        outputField.value = result += input[i].toUpperCase();
+      } else {
+        outputField.value = result += input[i];
+      }
+    });
     console.log(result);
   }
 }
