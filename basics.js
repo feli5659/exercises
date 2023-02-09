b7178ddfa77fc7e0dde66e4da3d06053ef41b8e5
felicia <feli5659@stud.kea.dk>
2023-02-09 10:35:13 +0100

    for loop exercises done

--- basics.js
+++ basics.js
@@ -167,32 +167,3 @@
 //   }
 // }
 
-// ---------------------  Loops -------------------
-
-// from 0 - 9
-// for (let counter = 0; counter < 10; counter++) {
-//   console.log(counter);
-// }
-
-// from 1 to 10
-// for (let counter = 1; counter < 11; counter++) {
-//   console.log(counter);
-// }
-// from 10 to 0 and a consolelog after 0 that saysf liftof
-for (let counter = 10; counter >= -1; counter--) {
-  if (counter < 0) {
-    console.log("liftoff");
-  } else {
-    console.log(counter);
-  }
-}
-// from 1 to 19 but only counts odd numbers (not sure how to only count odd numbers without if statement)
-// for (let counter = 1; counter < 20; counter++) {
-//   console.log(counter);
-// }
-
-// from 1 to 16777216, but doubling each time (2,4,8,16...)
-
-// for (let counter = 1; counter < 16777216; counter++ * 2) {
-//   console.log(counter);
-// }
